01fa9abaad3e1627040d00dc5f3bc329c7f17d4d
JeremyLin <ph804182010@gmail.com>
2019-05-16 22:03:00 +0800

    add JScommit

--- app.js
+++ app.js
@@ -1,69 +1,67 @@
 var vm = new Vue({
     el:'#app',
     data:{
-        time:71,
-        numOfItem:10,
-        sec60:false,
+        time:71,//倒數時間
+        numOfItem:10,//商品數量
+        sec60:false,//時間閥
         sec0:false,
-        btn:'購買',
-        boxNum:0,
-        boxCheck:false,
-        wantTo:'我要購買',
-        preOrder:0,
-        //0=normal,1=preOrder,2=over
+        btn:'購買',//按鈕名稱
+        boxNum:0,//購買數量
+        boxCheck:false,//彈窗開合
+        wantTo:'我要購買',//彈窗標題
+        preOrder:0,//預購數量
+        //模式變動0=normal(購買),1=preOrder(預購),2=over(停售)
         mode:0
     },
     mounted: function () {
         this.timeCount()
     },
     methods:{
-        timeCount: function(){
-            if(this.time>0){
+        timeCount: function(){//時間倒數
+            if(this.time>0){//歸零前
                 this_=this;
-                this_.time = this_.time - 1;
-                if(this_.time==59){
-                    this_.sec60 = true;
+                this.time = this.time - 1;
+                if(this.time===59){//59秒外框切換
+                    this.sec60 = true;
                 }
                 count = setTimeout(function(){this_.timeCount()},1000)
-            }else if(this.time==0){
+            }else if(this.time===0){//歸零
                 this.sec0 = true;
                 this.btn = '停售';
                 this.boxCheck = false;
-                this.mode = 2;
+                this.mode = 2;//切換模式2(停售)
             }
         },
-        numPlus: function(){
-            if(this.mode==0){
+        numPlus: function(){//button'+'
+            if(this.mode===0){//模式0(出售)狀態
                 if(this.boxNum<this.numOfItem){
                     this.boxNum = this.boxNum + 1;
                 }
-            }else if(this.mode==1){
+            }else if(this.mode===1){//模式1(預售)狀態
                 if(this.boxNum<99){
                     this.boxNum = this.boxNum + 1;
                 }
             }
         },
-        numLess: function(){
-            if(this.mode==0){
+        numLess: function(){//button'-'
                 if(this.boxNum>0){
                     this.boxNum = this.boxNum - 1;
                 }
-            }
         },
-        enterFn: function(){
-            if(this.mode==0){
+        enterFn: function(){//確認按鈕
+            if(this.mode===0){//模式0(出售)狀態
                 this.numOfItem = this.numOfItem - this.boxNum;
-            }else if(this.mode==1){
+            }else if(this.mode===1){//模式1(預售)狀態
                 this.preOrder = this.preOrder + this.boxNum;
             }
-            this.boxNum = 0;
-            if(this.numOfItem == 0){
+            this.boxNum = 0;//確認後欲購買數量歸零
+            if(this.numOfItem == 0){//若確認後商品數量為零，更換模式
                 this.btn = '預購';
                 this.wantTo = '我要預購';
                 this.mode = 1;
             }
         },
-        boxChecks: function(){
+        boxChecks: function(){//選單開合，判斷是否為模式2(停售)
             if(this.mode!=2){
                 if(this.boxCheck == true){
                     this.boxCheck = false;
